5af84d074ecb1839996a83f0920493fe4885cdc5
Your Name <you@example.com>
2023-07-29 07:55:11 +0200

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+import * as React from "react";
+
+import { ChainProvider } from "@cosmos-kit/react";
+import { chains, assets } from "chain-registry";
+import { wallets } from "@cosmos-kit/keplr";
+
+// Import this in your top-level route/layout
+import "@interchain-ui/react/styles";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ChainProvider
+        chains={chains} // supported chains
+        assetLists={assets} // supported asset lists
+        wallets={wallets} // supported wallets
+      >
+        <Component />
+      </ChainProvider>
+    </ChakraProvider>
+  );
 }
